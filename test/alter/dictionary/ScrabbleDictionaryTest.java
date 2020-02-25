53c80c521bd98d6271349552a71a851354327df0
shiraalter <shiraalter@gmail.com>
2020-02-25 10:08:20 -0500

    Scrabble Dictionary test

---
+++ test/alter/dictionary/ScrabbleDictionaryTest.java
@@ -0,0 +1,28 @@
+package alter.dictionary;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import static org.junit.Assert.*;
+
+public class ScrabbleDictionaryTest {
+    ScrabbleDictionary dictionary;
+
+    @Test
+    public void wordExists() throws IOException {
+         dictionary = new ScrabbleDictionary();
+         assertTrue(dictionary.wordExists("happy"));
+    }
+
+    @Test
+    public void wordDoesNotExist() throws IOException {
+        dictionary = new ScrabbleDictionary();
+        assertFalse(dictionary.wordExists("yo"));
+    }
+
+    @Test
+    public void caseInsensitivity() throws IOException {
+        dictionary = new ScrabbleDictionary();
+        assertTrue(dictionary.wordExists("HAPPY"));
+    }
+}
