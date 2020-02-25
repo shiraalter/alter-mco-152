c7599f5ff44047ed6af180473d1cafec33a03ebb
shiraalter <shiraalter@gmail.com>
2020-02-24 20:03:07 -0500

    added tests for exceptions

--- test/alter/cashier/CashierTest.java
+++ test/alter/cashier/CashierTest.java
@@ -7,9 +7,10 @@ import static org.junit.Assert.*;
 public class CashierTest {
     Cashier register;
     Cash cashInRegister;
+    Cash customer;
 
     @Test
-    public void pay() {
+    public void pay() throws NotEnoughChangeException, InsufficientPaymentException, BrokeCashierException {
         //given
         cashInRegister = new Cash();
         cashInRegister.setPennies(100);
@@ -33,21 +34,49 @@ public class CashierTest {
         assertEquals(0, customer.getOneDollars());
         assertEquals(2, customer.getQuarters());
         assertEquals(1, customer.getPennies());
+    }
 
-        //Test that no change is given if the amount paid < amount due:
+        @Test(expected = InsufficientPaymentException.class)
+        public void insufficientPayment() throws InsufficientPaymentException, NotEnoughChangeException, BrokeCashierException {
+
+            //given
+            cashInRegister = new Cash();
+            cashInRegister.setOneDollars(10);
+            register = new Cashier(cashInRegister); //set the register with cash object
+            double price = 5.00;
+            customer = new Cash();
+            customer.setOneDollars(3);
+
+            //when
+            register.pay(price, customer);
+
+        }
+
+    @Test(expected = BrokeCashierException.class)
+    public void brokeCashier() throws NotEnoughChangeException, InsufficientPaymentException, BrokeCashierException {
         //given
         cashInRegister = new Cash();
-        cashInRegister.setPennies(100);
-        cashInRegister.setQuarters(100);
-        register = new Cashier(cashInRegister);
-        customer.setOneDollars(1);
-        price = 3.00;
+        register = new Cashier(cashInRegister);  //empty cash object added
+        double price = 5.00;
+        customer = new Cash();
+        customer.setOneDollars(10);
 
         //when
-        Cash change = register.pay(price, customer);
-
-        //then
-        assertEquals(0, change.getPennies());
-        assertEquals(0, change.getQuarters());
+        register.pay(price, customer);
     }
-}
+
+    @Test(expected = NotEnoughChangeException.class)
+    public void notExactChange() throws BrokeCashierException,NotEnoughChangeException,InsufficientPaymentException {
+        //given
+        cashInRegister = new Cash();
+        cashInRegister.setTwentyDollars(1);
+        register = new Cashier(cashInRegister);
+        double price = 3.50;
+        customer = new Cash();
+        customer.setFiveDollars(1);     //need 1.50 change
+
+        //when
+       register.pay(price, customer);
+    }
+
+    }
