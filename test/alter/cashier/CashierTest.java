cf2a6fde3d958248a7af766f34cfde5b9a6063e0
shiraalter <shiraalter@gmail.com>
2020-03-02 14:30:10 -0500

    added test

--- test/alter/cashier/CashierTest.java
+++ test/alter/cashier/CashierTest.java
@@ -79,4 +79,42 @@ public class CashierTest {
        register.pay(price, customer);
     }
 
+    /*
+Write a test to make sure that if your Cashier.pay()
+method throws an Exception, that the money in the Cashier doesn't change.
+To do this you need to try-catch the Exception and then in the catch, make sure
+the Cashier's money is unchanged from what it was at the beginning.
+     */
+    @Test
+    public void exactPaymentEmptyRegister(){
+        //test that cashier updates/takes money when it starts off EMPTY and there is exact payment given
+            //even though there should be an exception thrown if the register is empty, it should update if no change is needed
+
+        //given
+        cashInRegister = new Cash();
+        register = new Cashier(cashInRegister); //set register with 0 dollars
+
+        double price = 3.00;
+        customer = new Cash();
+        customer.setOneDollars(3);
+
+        //when
+        try {
+            register.pay(price,customer);
+        } catch (NotEnoughChangeException e) {
+            e.printStackTrace();
+        } catch (InsufficientPaymentException e) {
+            e.printStackTrace();
+        } catch (BrokeCashierException e) {
+            e.printStackTrace();
+        }
+
+        //then
+        assertEquals(3.00, register.totalCash());
+
+
     }
+
+
+
+}
