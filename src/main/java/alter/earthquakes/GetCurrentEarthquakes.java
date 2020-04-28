871df3ae1ac18252090ca2defe58b5ec3faf6d0a
shiraalter <shiraalter@gmail.com>
2020-04-27 22:44:37 -0400

    GetCurrentEarthquakes

--- src/main/java/alter/earthquakes/GetCurrentEarthquakes.java
+++ src/main/java/alter/earthquakes/GetCurrentEarthquakes.java
@@ -1,20 +1,46 @@
 package alter.earthquakes;
 
-import com.google.gson.Gson;
-//import alter.weather.CurrentWeather;
 
-import java.io.BufferedReader;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-//import java.net.MalformedURLException;
-import java.net.URL;
+
 
 public class GetCurrentEarthquakes {
 
     public static void main(String[] args) throws IOException {
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://earthquake.usgs.gov/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        EarthquakeService service = retrofit.create(EarthquakeService.class);
+
+        service.getThisMonth().enqueue(new Callback<CurrentEarthquakes>() {
+            @Override
+            public void onResponse(Call<CurrentEarthquakes> call, Response<CurrentEarthquakes> response) {
+                CurrentEarthquakes currentEarthquakes = response.body();
+                for (CurrentEarthquakes.Feature feature : currentEarthquakes.features) {
+                    System.out.println(feature.properties.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CurrentEarthquakes> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+        }
+
+
+
+
+      /*  above statements do the following:
         URL url = new URL("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         InputStream in = connection.getInputStream();
@@ -22,10 +48,4 @@ public class GetCurrentEarthquakes {
 
         Gson gson = new Gson();
         CurrentEarthquakes currentEarthquakes = gson.fromJson(reader, CurrentEarthquakes.class);
-
-        for (CurrentEarthquakes.Feature feature : currentEarthquakes.features) {
-            System.out.println(feature.properties.toString());
-        }
-    }
-
-}
+        */
