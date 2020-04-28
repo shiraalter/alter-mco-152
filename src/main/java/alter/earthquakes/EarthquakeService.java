b6dddb7949ec2f00c5771de667f19c496de43910
shiraalter <shiraalter@gmail.com>
2020-04-27 22:45:10 -0400

    EarthquakeService

---
+++ src/main/java/alter/earthquakes/EarthquakeService.java
@@ -0,0 +1,9 @@
+package alter.earthquakes;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface EarthquakeService {
+    @GET("earthquakes/feed/v1.0/summary/significant_month.geojson")
+    Call<CurrentEarthquakes> getThisMonth();
+}
